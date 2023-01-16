414ce45fb451d4ea310609348503245064d75d0e
Emestay <103575914+Emestay@users.noreply.github.com>
2023-01-16 09:28:04 +0100

    code basqieu ToDo avec localStorage

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-let arrayToDo = [];
+document.querySelector("form").addEventListener("submit", addTodo);
+document.addEventListener("DOMContentLoaded", getTodoList);
+let todoList = [];
 
 
+function addTodo(event) {
+    event.preventDefault();
+    let todo = document.querySelector("input").value;
+    todoList.push(todo);
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+    displayTodoList();
+}
+
+function deleteUnTodo(event){
+    
+}
+
+function getTodoList() {
+    if (localStorage.getItem("todoList")) {
+        todoList = JSON.parse(localStorage.getItem("todoList"));
+        displayTodoList();
+    }
+}
+
+
+function displayTodoList() {
+    let list = document.querySelector("#todo-list");
+    list.innerHTML = "";
+    for (let i = 0; i < todoList.length; i++) {
+        let item = document.createElement("li");
+        item.innerHTML = todoList[i];
+        list.appendChild(item);
+    }
+}
